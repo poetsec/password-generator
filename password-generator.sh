f8e989e9a2bfbd631825b7d39deb1edbd4a631fb
Taylor Shakespear <52114607+poetsec@users.noreply.github.com>
2021-07-19 23:36:48 -0600

    Update password-generator.sh

--- password-generator.sh
+++ password-generator.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-echo "Welcome to the password generator."
+
+PURPLE='\033[0;35m'
+GREEN='\033[0;32m'
+NC='\033[0m' # no color
+
+echo -e "${PURPLE}###########################################"
+echo -e "####     Random Password Generator     ####"
+echo -e "###########################################"
+echo -e "####             @poetsec              ####"
+echo -e "###########################################${NC}"
+
+echo -e "${GREEN}Welcome to the password generator.${NC}"
 echo "How many characters would you like your new password to have?"
 read pass_length
 
